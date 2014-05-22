cdee74832fdb7734659e2a62cf50bb10c5c1df60
James <jagleeso@gmail.com>
2014-05-21 20:29:49 -0400

    whole buncha bin's

---
+++ bin/zsh/uoftpc/common.sh
@@ -0,0 +1,58 @@
+view_errors() {
+    local file="$1"
+    shift 1
+    vim "$file" -c 'vimgrep /\c\(err\|reboot\|bug\|Kernel panic\)/ % | copen'
+}
+
+view_with_pattern() {
+    local file="$1"
+    local pattern="$2"
+    shift 2
+    vim "$file" -c "vimgrep /$pattern/ % | copen"
+}
+
+cat_and_view_errs() {
+    local remote="$1"
+    local local="$2"
+    adb_sh cat "$remote" > "$local"
+    view_errors "$local"
+}
+
+
+adb_sudo() {
+    adb_sudo_aosp "$@"
+    # adb_sudo_supersu "$@"
+}
+adb_sudo_supersu() {
+    # works with the su command installed by SuperSu
+    adb shell 'su -c "'"$@"'"'
+}
+adb_sudo_aosp() {
+    # works with the su command bundled in the AOSP (system/extras/su/su.c)
+    # (installed when building via "lunch full_mako-userdebug")
+    adb shell 'su 0 sh -c "'"$@"'"'
+}
+
+adb_sh() {
+    adb_sudo "$@"
+}
+
+wait_for_bootloader() {
+    while true; do
+        if fastboot devices | grep --quiet 'fastboot$'; then
+            fastboot devices
+            break
+        fi
+        sleep 1
+    done
+}
+
+wait_for_boot() {
+    while true; do
+        if adb devices | grep --quiet 'device$'; then
+            adb devices
+            break
+        fi
+        sleep 1
+    done
+}
