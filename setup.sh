7c2447339ac87090cd4816c968c875dc74746074
James <jagleeso@gmail.com>
2017-02-11 18:44:10 -0500

    .

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
-# set -x
+if [ "$DEBUG" = 'yes' ]; then
+    set -x
+fi
 
 # Hopefully the most stable one.
 VIM_TAG="v8.0.0000"
+VIM_PY2_CONFIG_DIR=/usr/lib/python2.7/config
+VIM_PY3_CONFIG_DIR=/usr/lib/python2.7/config
 
 # Force re-running setup
 if [ "$FORCE" = "" ]; then
@@ -49,6 +53,7 @@ setup_vim() {
     if [ "$FORCE" != 'yes' ] && [ -f $HOME/local/bin/vim ]; then
         return
     fi
+
     REINSTALLED_VIM=yes
     mkdir -p $HOME/local
     mkdir -p $HOME/clone
@@ -56,35 +61,47 @@ setup_vim() {
         (
             cd $HOME/clone
             git clone https://github.com/vim/vim.git
+            git checkout $VIM_TAG
         )
     else
         (
             cd $HOME/clone/vim
-            git pull
+            # make clean
+            git fetch
             git checkout $VIM_TAG
         )
     fi
+
     _install \
         libncurses5-dev libgnome2-dev libgnomeui-dev \
         libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
         libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
         python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+
     (
         cd $HOME/clone/vim
+        # --enable-pythoninterp=yes
+        # http://stackoverflow.com/questions/23023783/vim-compiled-with-python-support-but-cant-see-sys-version
+        #
+        # TLDR: 
+        # Linux vim cannot load both python2 and python3, causing them to both be loaded 
+        # dynamically, which makes YCM angry.
+        #
+        # Solution: 
+        # use only one, disable the other.
         ./configure --with-features=huge \
             --enable-multibyte \
             --enable-rubyinterp=yes \
-            --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config \
+            --with-python-config-dir=$VIM_PY2_CONFIG_DIR \
             --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.5/config \
+            --with-python3-config-dir=$VIM_PY3_CONFIG_DIR \
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
             --enable-gui=gtk2 \
             --enable-cscope \
             --prefix=$HOME/local
         make -j$NCPU install
-        vim -c PluginInstall -c quit -c quit
+        vim -c PluginInstall -c PluginUpdate -c quit -c quit
     )
 }
 setup_ycm_before() {
@@ -104,7 +121,7 @@ setup_vim_after() {
     ln -s $HOME/.vim/bundle/YCM-Generator/config_gen.py . || true
 }
 setup_packages() {
-    _install htop zsh tree clang
+    _install htop zsh tree clang silversearcher-ag
 }   
 setup_fzf() {
     if [ "$FORCE" != 'yes' ] && [ -d $HOME/.fzf ]; then
