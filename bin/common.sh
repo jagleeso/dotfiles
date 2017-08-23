817d774b1a4bde4e25d213fb78fd731769295f5e
James <jagleeso@gmail.com>
2017-08-23 09:07:44 -0400

    .

--- bin/common.sh
+++ bin/common.sh
@@ -18,6 +18,39 @@ kwin() {
     done
 }
 
+tunnel_everything() {
+    local local_port="$1"
+    local remote_syslab_port="$2"
+    local remote_xen1_port="$3"
+    shift 3
+    ssh -v \
+        -L$local_port:localhost:$remote_syslab_port syslab -t ssh -v \
+        -L$remote_syslab_port:localhost:$remote_xen1_port \
+        james@10.70.2.2
+}
+
+tunnel_only_to_syslab() {
+    local local_port="$1"
+    local remote_syslab_port="$2"
+    local remote_xen1_port="$3"
+    shift 3
+    # NOTE: this assumes a tunnel is already setup on syslab.
+    syslab_is_tunneling_to_xen1() {
+        # Make sure this is running:
+        # ssh -v -L 8787:localhost:22 james@10.70.2.2
+        ssh syslab "ps aux | grep -v grep | grep -q 'ssh.*-L.*$remote_syslab_port'"
+    }
+    if ! syslab_is_tunneling_to_xen1; then
+        echo "ERROR: You need to login to syslab and tunnel from syslab to xen1:"
+        echo "  $ ssh syslab"
+        echo "  $ ssh -v -L $remote_syslab_port:localhost:$remote_xen1_port james@10.70.2.2"
+        exit 1
+    fi
+    # Try using autossh locally to keep connection alive.
+    autossh -v \
+        -L $local_port:localhost:$remote_syslab_port syslab
+}
+
 if [ "$RUN_COMMON" == "yes" ]; then
     "$@"
 fi
